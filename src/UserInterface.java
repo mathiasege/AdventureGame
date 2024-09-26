f74c1fa4bf171b45e0c8d6fcab23ec991e47fbd7
mathiasege <macl0001@stud.kea.dk>
2024-09-26 09:40:35 +0200

    Ændret et variabel navn

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -5,25 +5,25 @@ public class UserInterface {
     public void game(){
         Scanner scan = new Scanner(System.in);
         Adventur adventur = new Adventur();
-        String move = "";
+        String input = "";
 
         System.out.println("You're in room: " + adventur.getRoomName());
         System.out.println(adventur.getDescription());
 
-        while(!move.equals("exist")){
+        while(!input.equals("exist")){
             System.out.println("(1) Move command Type in: North, East, West, South.");
             System.out.println("(2) Looking in room type in: look.");
             System.out.println("(3) Need help Type in: help.");
             System.out.println("(4) If you want to quiet game. Type in: exist.");
 
-            move = scan.nextLine().toLowerCase();
-            switch (move){
+            input = scan.nextLine().toLowerCase();
+            switch (input){
                 case "north", "south", "east", "west" -> {
                     // Tjekker om det er muligt at gå igennem.
-                    if(adventur.checkLock(move) && adventur.getRoomName() != null)
+                    if(adventur.checkLock(input) && adventur.getRoomName() != null)
                         System.out.println("The door is locked. Find another way");
                     else
-                        System.out.println(adventur.move(move));
+                        System.out.println(adventur.move(input));
                 }
                 case "look" -> System.out.println(adventur.checkLocations());
                 case "exist" -> System.out.println("You will now exist game.");
